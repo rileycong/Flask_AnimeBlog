96b9208641b752212ba09154f07532c975f5f148
Chau Anh <chauanhcong@gmail.com>
2021-12-18 15:20:07 +0700

    video_2

--- flask_webapp.py
+++ flask_webapp.py
@@ -1,17 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Wibu 1',
+        'title': 'GTO and the state of adulthood',
+        'content': 'Basically a great show!',
+        'date_posted': 'April 20, 2017'
+    },
+    {
+        'author': 'Protagonist wannabe',
+        'title': 'Spoilers for Made in Abyss',
+        'content': 'Another day another spoiler',
+        'date_posted':'April 21, 2017' 
+    },
+    {
+        'author': 'hMmn',
+        'title': 'Dead dead demon and the Isino humor',
+        'content': 'Too busy monitoring the internet',
+        'date_posted':'July 21, 2020' 
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Home page</h1>"
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About the noob behind this</h1>"
+    return render_template('about.html', title = 'About')
 
 @app.route("/rate")
 def rate():
-    return "<h1>Rate Anime</h1>"
+    return render_template('rate.html', title = 'Rate')
 
 if __name__ == '__main__':
     app.run(debug=True)
