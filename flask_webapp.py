081d1cb6d30c5a7bfdfe490b543289c967bdd1d2
Chau Anh <chauanhcong@gmail.com>
2021-12-19 21:30:02 +0700

    video 3

--- flask_webapp.py
+++ flask_webapp.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd7ae971563dcb9e66deafc03276d6ac8'
 
 posts = [
     {
@@ -33,6 +36,25 @@ def about():
 @app.route("/rate")
 def rate():
     return render_template('rate.html', title = 'Rate')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == '123':
+            flash('Okay bro you are in the hood', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('LOL you failed even the simplist stuff. Check again mate!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
