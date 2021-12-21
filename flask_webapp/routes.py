7d01d812684405595dd87e2f79cea9f0846dd3f3
Chau Anh <chauanhcong@gmail.com>
2021-12-21 14:05:48 +0700

    video 7

--- flask_webapp/routes.py
+++ flask_webapp/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_webapp import app, db, bcrypt
-from flask_webapp.forms import RegistrationForm, LoginForm
+from flask_webapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_webapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -67,7 +70,38 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            old_pic = current_user.image_file
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+            if old_pic !='default.jpg':
+                os.remove(os.path.join(app.root_path, 'static/profile_pics', old_pic))
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Successfully reincarnated, hooray !', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
